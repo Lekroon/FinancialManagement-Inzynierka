d61afaa585a7f85b396ff6483e94ca789f2ef972
bartan-tm <bartan003@pbs.edu.pl>
2023-08-11 12:03:34 +0200

    created views for login, and temporary users list view to check if everything works correctly.

---
+++ FinancialManagement/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialManagement.Controllers;
+
+[Route("user")]
+public class UserController : Controller
+{
+    [Route("login")]
+    [Route("/")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [Route("temp/users")]
+    public IActionResult Users()
+    {
+        return View();
+    }
+}
